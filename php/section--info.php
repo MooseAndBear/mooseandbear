387a7aff52201efbab4fae44168fe2185dd3d2cd
BuildTools <unconfigured@null.spigotmc.org>
2015-12-22 23:07:57 +0000

    Testing

--- php/section--info.php
+++ php/section--info.php
@@ -18,7 +18,7 @@
           <h1>More Info</h1>
           <br />
           <div class="para-wrapper">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eveniet beatae quisquam facere non impedit iste possimus, animi repellendus saepe fuga repudiandae officiis! Id amet reprehenderit, sunt accusantium enim velit.</p>
+            <p>lolololololoolololololo, eveniet beatae quisquam facere non impedit iste possimus, animi repellendus saepe fuga repudiandae officiis! Id amet reprehenderit, sunt accusantium enim velit.</p>
             <p>Consectetur adipisicing elit. Quos officia fugiat nisi quod quasi laboriosam incidunt ea corporis fuga provident consectetur, deserunt saepe odit repudiandae est dolore rerum unde. Nisi.</p>
             <br />
             <a class="-js-slide"><h3>Go Back</h3></a>
